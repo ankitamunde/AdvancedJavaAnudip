e5256ba7521e209c8c9b9e7b596d66aca11e1f5d
Ankita Munde <160840863+ankitamunde@users.noreply.github.com>
2025-03-04 18:15:25 +0530

    Final Commit

---
+++ StudentsManagementSystem/src/main/java/com/anudip/sms/model/Student.java
@@ -0,0 +1,23 @@
+package com.anudip.sms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+	public Student(long l, String string, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
